bdb8e3560d6c9754723e17c2e7a7d9e2cb7daa1d
mahmoodhamd <116299051+mahmoodhamd@users.noreply.github.com>
2023-11-29 13:32:33 +0500

    Initial commit

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,30 @@
+<?php
+namespace Database\Factories;
+
+use App\Models\Book;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        return [
+            'title'=>$this->faker->sentence(3),
+            'author'=>$this->faker->name(3),
+            'created_at'=>$this->faker->dateTimeBetween('-2 years'),
+            'updated_at'=>$this->faker->dateTimeBetween('created_at','now'),
+
+
+        ];
+    }
+}
