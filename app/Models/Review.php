55d196071f7434162a7c433cd89949ebdba8a0f3
mahmoodhamd <116299051+mahmoodhamd@users.noreply.github.com>
2023-12-05 16:56:51 +0500

    changes in the models and did some tinker code

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
     use HasFactory;
-    public function book(){
+    protected $fillable = ['review','rating'];
+
+    public function book()
+    {
        return $this->belongsTo(Book::class);
     }
 }
